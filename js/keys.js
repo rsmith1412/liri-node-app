0a0d830ff16ff9bc83603b4cf6e0d7c0ae2e87e7
Robert Smith <robertsmith1@Roberts-MacBook-Pro.local>
2017-03-31 23:55:37 -0700

    initial site files

---
+++ js/keys.js
@@ -0,0 +1,14 @@
+//So we now the file is working
+console.log('this is loaded');
+
+//Twitter API keys to be used later
+exports.twitterKeys = {
+  consumer_key: 'BDvjHqUImiauPgoIoxJkbxIc8',
+  consumer_secret: 'd0gR8qJsfPeAEmnrHKniinN8fNbrutVeIbJ08hfHPpTwEg5K9o',
+  access_token_key: '847212683526946817-l7UoulyMaUQ5Vphq6mOi1JUwjEXyIgT',
+  access_token_secret: '3rxFn6ZcMohnNgq4T0y3qtvlGQ3Mf0OdoLQP3chFHlcwu',
+}
+
+exports.spotifyKeys = {
+    
+}
---
+++ js/liri.js
@@ -0,0 +1,8 @@
+
+//LIRI will take arguments and produce data with them
+
+var keys = require("./keys.js");
+var fs = require('fs');
+var twitter = require ('twitter');
+var spotify = require ('spotify');
+var request = require ('twitter');
